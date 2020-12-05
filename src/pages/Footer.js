3f0f0a82122f0dfca2538117eac3bb0f53033cbc
Ayush Kaushik <anon.programmer305@gmail.com>
2020-12-05 10:30:20 -0500

    FOOTER: Improved footer layout

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {DEVTO_URL, LINKEDIN_URL, GITHUB_URL} from "../constants/Footer";
+import {DEVTO_URL, LINKEDIN_URL, GITHUB_URL, COPYRIGHT} from "../constants/Footer";
 
 const Footer = () => {
     return (
-        <div className={"layout-footbar"}>
+        <div className={"layout-footer"}>
             <ul>
                 <li><a href={GITHUB_URL}>{"Github"}</a></li>
                 <li><a href={LINKEDIN_URL}>{"LinkedIn"}</a></li>
                 <li><a href={DEVTO_URL}>{"Dev.to"}</a></li>
             </ul>
+            <div>
+                <p>{COPYRIGHT}</p>
+            </div>
         </div>
     )
 };
